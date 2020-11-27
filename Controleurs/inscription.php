32e2aa92ddd735f74cb72810b2d48b6cd12bd3d5
Georges <georges.guitton5@gmail.com>
2020-11-27 09:42:54 +0100

    type mail fait & vérif mail unique qui semi marche

--- Controleurs/inscription.php
+++ Controleurs/inscription.php
@@ -2,7 +2,7 @@
     session_start();
     require_once("../modeles/bd.php");
     require_once("../modeles/utilisateur.php");
-    
+
     if (isset($_POST["login"]) && isset($_POST["mdp"]) && isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["statut"]))
     {
         $login = $_POST["login"];
@@ -10,9 +10,30 @@
         $nom = $_POST["nom"];
         $prenom=$_POST["prenom"];
         $statut=$_POST["statut"];
-        $coBd = new bd("efreinnovation");
-        $co = $coBd->connexion();
-        $m = new utilisateur($co,$login,$mdp,$nom,$prenom,$statut);
-    }
-        header('Location:../Vues/connexion.html');
+
+        $coBd= new bd("efreinnovation");
+        $co=$coBd->connexion() or die("Erreur connexion");
+
+        $result=mysqli_query($co,"SELECT login FROM utilisateur WHERE login='$login'")
+        or die("Erreur requete connexion");
+
+        if(mysqli_num_rows($result)==1)
+        { 
+            ?>
+            <script>
+                alert("Ce mail est deja utilisé");
+            </script>
+            <?php
+        }
+        else
+        {
+            $coBd = new bd("efreinnovation");
+            $co = $coBd->connexion();
+            $m = new utilisateur($co,$login,$mdp,$nom,$prenom,$statut);
+            header('Location:../Vues/connexion.html');
+        }
+    } 
 ?>
+
+
+
